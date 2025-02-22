f9c0af2b404ba720b0a28b9a813caf6230166899
Nguyễn Anh Duy <duynguyenbt21093@gmail.com>
2025-02-22 14:27:10 +0700

    0290 WordPattern

---
+++ solutions/solutions/Easy/0290 WordPattern.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Easy
+{
+    public class Solution_0290
+    {
+        public bool WordPattern(string pattern, string s)
+        {
+            string[] words = s.Split(' ');
+            if(pattern.Length != words.Length)
+            {
+                return false;
+            }
+            Dictionary<char, string> dic = new Dictionary<char, string>();
+            for(int i = 0; i<pattern.Length; i++)
+            {
+                if (dic.ContainsKey(pattern[i])){
+                    if (dic[pattern[i]] != words[i])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (!dic.ContainsValue(words[i])){
+                        dic.Add(pattern[i], words[i]);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    
+                    
+                }
+            }
+            return true;
+        }
+    }
+    public class _0290_WordPattern
+    {
+        public static void Test()
+        {
+            Solution_0290 solution = new Solution_0290();
+
+            Console.WriteLine(solution.WordPattern("abba", "dog dog dog dog")); // false
+            Console.WriteLine(solution.WordPattern("abba", "dog cat cat dog")); // true
+            Console.WriteLine(solution.WordPattern("abba", "dog cat cat fish")); // false
+            
+        }
+    }
+}
--- solutions/solutions/Program.cs
+++ solutions/solutions/Program.cs
@@ -3,4 +3,4 @@ using Leetcode.Interview.NashTech;
 using Leetcode.Medium;
 using solutions.Easy;
 
-_0242_ValidAnagram.Test();
+_0290_WordPattern.Test();
