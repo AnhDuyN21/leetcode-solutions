783ad354b96c73dc7400bfdb58e276f3be75713f
Nguyễn Anh Duy <duynguyenbt21093@gmail.com>
2025-02-22 13:56:37 +0700

    0242 ValidAnagram

---
+++ solutions/solutions/Easy/0242 ValidAnagram.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Easy
+{
+    public class Solution_0242
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            char[] sArray = s.ToCharArray();
+            char[] tArray = t.ToCharArray();
+            Array.Sort(sArray);
+            Array.Sort(tArray);
+            string sortedS = new string(sArray);
+            string sortedT = new string(tArray);
+            if (sortedS == sortedT)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+    public class _0242_ValidAnagram
+    {
+        public static void Test()
+        {
+            Solution_0242 solution = new Solution_0242();
+
+            Console.WriteLine(solution.IsAnagram("anagram", "nagaram")); // true
+
+            Console.WriteLine(solution.IsAnagram("rat", "car")); // false
+        }   
+    }
+}
--- solutions/solutions/Program.cs
+++ solutions/solutions/Program.cs
@@ -3,4 +3,4 @@ using Leetcode.Interview.NashTech;
 using Leetcode.Medium;
 using solutions.Easy;
 
-_0205_IsomorphicStrings.Test();
+_0242_ValidAnagram.Test();
